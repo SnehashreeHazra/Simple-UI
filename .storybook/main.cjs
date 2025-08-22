5c775de454d1de3d509269dc8f7e08396bdcd7c7
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-08-22 14:33:46 +0530

    'up'

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,11 +2,16 @@ const path = require('path');
 const tsconfigPaths = require('vite-tsconfig-paths');
 
 module.exports = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
-  framework: '@storybook/react-vite',
-  viteFinal: async (config) => {
-    config.plugins?.push(tsconfigPaths());
-    return config;
+   stories: [
+    '../src/components/**/**/*.stories.@(ts|tsx|js|jsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
 };
